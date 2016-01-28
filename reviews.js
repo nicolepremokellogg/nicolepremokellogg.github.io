ef7ba55cf9aea2ab261054447b06a2c36b589a3f
nicolepremokellogg <nicolepremo@nicolepremo-macbookpro2.roam.corp.google.com>
2016-01-28 14:12:16 -0600

    First pass

---
+++ reviews.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".btn").on("click", addReview);
+  $(".list-group").on("click", ".link-delete", deleteReview);
+});
+
+function addReview() {
+  var review = window.prompt("Submit your review of Laudable Coffee.");
+  var delete_link = '<a href="#" class="link-delete">(Delete your review)</a>'
+  $(".list-group").append("<li class='list-group-item'>" + review + " " + delete_link + "</li>");
+  numReviews = $("li").length;
+  $(".total").html(numReviews + " reviews");
+
+  if (numReviews == 1) {
+    $(".total").html(numReviews + " review");
+  }
+  else {
+    $(".total").html(numReviews + " reviews");
+  }
+}
+
+function deleteReview(event) {
+  $(event.target).parent().remove();
+  numReviews = $("li").length;
+  $(".total").html(numReviews + " reviews")
+}
