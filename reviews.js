eb2afef6c6c1a5509c138530daa06c1165566957
nicolepremokellogg <nicolepremo@nicolepremo-macbookpro2.roam.corp.google.com>
2016-02-08 10:04:26 -0600

    Update to "review" count

--- reviews.js
+++ reviews.js
@@ -7,20 +7,20 @@ function addReview() {
   var review = window.prompt("Submit your review of Laudable Coffee.");
   var delete_link = '<a href="#" class="link-delete">(Delete your review)</a>'
   $(".list-group").append("<li class='list-group-item'>" + review + " " + delete_link + "</li>");
-  // numReviews = $("li").length;
-  // $(".total").html(numReviews + " reviews");
+  numReviews = $(".list-group-item").length;
+  $(".total").html(numReviews + " reviews");
   ;
 
-  // if (numReviews == 1) {
-  //   $(".total").html(numReviews + " review");
-  // }
-  // else {
-  //   $(".total").html(numReviews + " reviews");
-  // }
+  if (numReviews == 1) {
+    $(".total").html(numReviews + " review");
+  }
+  else {
+    $(".total").html(numReviews + " reviews");
+  }
 }
 
 function deleteReview(event) {
   $(event.target).parent().remove();
-  // numReviews = $("li").length;
-  // $(".total").html(numReviews + " reviews")
+  numReviews = $(".list-group-item").length;
+  $(".total").html(numReviews + " reviews")
 }
